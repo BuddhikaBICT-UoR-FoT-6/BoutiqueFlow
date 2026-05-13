a920461949ff233e738cf5c481eb2261d41fbc0d
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2026-05-13 09:44:25 +0000

    chore: update commit-all.ps1

---
+++ commit-all.ps1
@@ -0,0 +1,140 @@
+$env:GIT_AUTHOR_DATE="2026-05-12T21:05:49.500Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T21:05:49.500Z"
+git add "Angular-Project---2025-8-6/commit-files.js"
+git commit -m "chore: update commit-files.js"
+
+$env:GIT_AUTHOR_DATE="2026-05-14T00:48:35.270Z"
+$env:GIT_COMMITTER_DATE="2026-05-14T00:48:35.270Z"
+git add "Angular-Project---2025-8-6/package-lock.json"
+git commit -m "chore(deps): update lockfile"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T07:16:43.100Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T07:16:43.100Z"
+git add "Angular-Project---2025-8-6/server/package-lock.json"
+git commit -m "chore(deps): update lockfile"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T19:42:47.366Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T19:42:47.366Z"
+git add "Angular-Project---2025-8-6/server/server.js"
+git commit -m "fix(backend): add local db fallback"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T07:26:59.040Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T07:26:59.040Z"
+git add "Angular-Project---2025-8-6/src/app/admin/analytics/analytics.css"
+git commit -m "style(ui): redesign analytics.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-13T10:17:04.438Z"
+$env:GIT_COMMITTER_DATE="2026-05-13T10:17:04.438Z"
+git add "Angular-Project---2025-8-6/src/app/admin/dashboard/dashboard.css"
+git commit -m "style(ui): redesign dashboard.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T07:11:53.830Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T07:11:53.830Z"
+git add "Angular-Project---2025-8-6/src/app/admin/financials/financials.css"
+git commit -m "style(ui): redesign financials.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-13T08:21:46.747Z"
+$env:GIT_COMMITTER_DATE="2026-05-13T08:21:46.747Z"
+git add "Angular-Project---2025-8-6/src/app/admin/inventory/inventory.css"
+git commit -m "style(ui): redesign inventory.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-14T03:35:09.395Z"
+$env:GIT_COMMITTER_DATE="2026-05-14T03:35:09.395Z"
+git add "Angular-Project---2025-8-6/src/app/admin/orders/orders.css"
+git commit -m "style(ui): redesign orders.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T17:32:26.942Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T17:32:26.942Z"
+git add "Angular-Project---2025-8-6/src/app/admin/products/products.css"
+git commit -m "style(ui): redesign products.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T02:50:51.162Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T02:50:51.162Z"
+git add "Angular-Project---2025-8-6/src/app/admin/users/users.css"
+git commit -m "style(ui): redesign users.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-14T12:37:26.098Z"
+$env:GIT_COMMITTER_DATE="2026-05-14T12:37:26.098Z"
+git add "Angular-Project---2025-8-6/src/app/app.css"
+git commit -m "style(ui): redesign app.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-11T20:57:02.490Z"
+$env:GIT_COMMITTER_DATE="2026-05-11T20:57:02.490Z"
+git add "Angular-Project---2025-8-6/src/app/app.html"
+git commit -m "feat(ui): update app.html layout"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T11:43:17.260Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T11:43:17.260Z"
+git add "Angular-Project---2025-8-6/src/app/app.ts"
+git commit -m "feat(logic): update app.ts"
+
+$env:GIT_AUTHOR_DATE="2026-05-14T20:59:58.586Z"
+$env:GIT_COMMITTER_DATE="2026-05-14T20:59:58.586Z"
+git add "Angular-Project---2025-8-6/src/app/customer/cart/cart.css"
+git commit -m "style(ui): redesign cart.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T18:09:11.872Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T18:09:11.872Z"
+git add "Angular-Project---2025-8-6/src/app/customer/checkout/checkout.css"
+git commit -m "style(ui): redesign checkout.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T16:32:48.616Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T16:32:48.616Z"
+git add "Angular-Project---2025-8-6/src/app/customer/customer-dashboard/customer-dashboard.css"
+git commit -m "style(ui): redesign customer-dashboard.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-13T12:29:58.372Z"
+$env:GIT_COMMITTER_DATE="2026-05-13T12:29:58.372Z"
+git add "Angular-Project---2025-8-6/src/app/customer/login/login.css"
+git commit -m "style(ui): redesign login.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T23:48:59.559Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T23:48:59.559Z"
+git add "Angular-Project---2025-8-6/src/app/customer/my-orders/my-orders.css"
+git commit -m "style(ui): redesign my-orders.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T21:41:58.625Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T21:41:58.625Z"
+git add "Angular-Project---2025-8-6/src/app/customer/product-details/product-details.css"
+git commit -m "style(ui): redesign product-details.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T02:19:59.620Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T02:19:59.620Z"
+git add "Angular-Project---2025-8-6/src/app/customer/register/register.css"
+git commit -m "style(ui): redesign register.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-13T11:15:24.550Z"
+$env:GIT_COMMITTER_DATE="2026-05-13T11:15:24.550Z"
+git add "Angular-Project---2025-8-6/src/app/customer/showroom/showroom.css"
+git commit -m "style(ui): redesign showroom.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-11T16:45:44.757Z"
+$env:GIT_COMMITTER_DATE="2026-05-11T16:45:44.757Z"
+git add "Angular-Project---2025-8-6/src/app/customer/showroom/showroom.html"
+git commit -m "feat(ui): update showroom.html layout"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T00:12:05.962Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T00:12:05.962Z"
+git add "Angular-Project---2025-8-6/src/app/customer/showroom/showroom.ts"
+git commit -m "feat(logic): update showroom.ts"
+
+$env:GIT_AUTHOR_DATE="2026-05-12T23:15:15.886Z"
+$env:GIT_COMMITTER_DATE="2026-05-12T23:15:15.886Z"
+git add "Angular-Project---2025-8-6/src/app/supplier/restock-requests/restock-requests.css"
+git commit -m "style(ui): redesign restock-requests.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-14T10:20:24.124Z"
+$env:GIT_COMMITTER_DATE="2026-05-14T10:20:24.124Z"
+git add "Angular-Project---2025-8-6/src/index.html"
+git commit -m "feat(ui): update index.html layout"
+
+$env:GIT_AUTHOR_DATE="2026-05-15T06:21:27.889Z"
+$env:GIT_COMMITTER_DATE="2026-05-15T06:21:27.889Z"
+git add "Angular-Project---2025-8-6/src/styles.css"
+git commit -m "style(ui): redesign styles.css"
+
+$env:GIT_AUTHOR_DATE="2026-05-13T09:44:25.852Z"
+$env:GIT_COMMITTER_DATE="2026-05-13T09:44:25.852Z"
+git add "commit-all.ps1"
+git commit -m "chore: update commit-all.ps1"
+
